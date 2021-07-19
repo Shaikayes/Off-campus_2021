410b0b89c635eb36346f691644079d0edda4b943
Shaik Ayesha <86394750+Shaikayes@users.noreply.github.com>
2021-07-19 22:21:56 +0530

    Add files via upload

---
+++ spring-quickstart/src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	int productId;
+	String productName;
+	double ratePerUnit;
+
+}
